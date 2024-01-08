ac7a55464ae3e63f5ce0dcfa0b6b7eac4202f269
Youssef El Idrissi <professionalidrissi@gmail.com>
2024-01-08 03:15:19 +0100

    First Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let dealer_cards = document.querySelectorAll(".dealers-cards-container .card");
+for (card of dealer_cards) {
+    card.style['backgroundImage'] = "url('assets/c/3.png')"
+}
+    
+let player_cards = document.querySelectorAll(".players-cards-container .card");
+for (card of player_cards) {
+    card.style['backgroundImage'] = "url('assets/s/K.png')"
+}
